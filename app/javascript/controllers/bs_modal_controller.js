462d5e1985f2b8dcdaebfc1099c4e7a902c7d35f
AqsaAltaf1 <AqsaAltaf1@github.com>
2023-08-04 11:23:26 +0500

    modal change

--- app/javascript/controllers/bs_modal_controller.js
+++ app/javascript/controllers/bs_modal_controller.js
@@ -4,14 +4,28 @@ import { Modal } from "bootstrap";
 // Connects to data-controller="bs-modal"
 export default class extends Controller {
   connect() {
+    console.log('hii')
     this.modal=new Modal(this.element)
     this.modal.show()
   }
+
+  show(event){
+    if (this.open(event)){
+      event.preventDefault()
+      this.element.addEventListener("hidden.bs.modal", event.detail.resume)
+      this.modal.hide()
+    }
+  }
+ 
   disconnect(){
     this.modal.hide()
   }
-  submitEnd(event){
+  submitEnd(){
     this.modal.hide()
+  }
+
+  open(){
+    return this.element.classList.contains('show')
   }
 
 }
